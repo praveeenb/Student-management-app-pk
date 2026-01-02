d86cd61d565db6f157720fcd7a3c82b70e3390e9
praveeenb <praveen54232432@gmail.com>
2026-01-02 13:56:20 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+app.use(express.json());
+
+// Import routes
+const studentRoutes = require('./routes/studentRoutes');
+app.use('/students', studentRoutes);
+
+// Test route
+app.get('/', (req, res) => {
+    res.send('Student API is running successfully!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
